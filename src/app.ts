5f5e02a9521f93ed118477bb6740b813d9fb50de
Judith Fuog <lyriael@gmx.net>
2020-05-05 16:00:38 +0200

    TS-Basics: 7. Arrow functions

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log('Ahoy TypeScript!');
+const pizzas = [
+    { name: 'Pepperoni', toppings: ['pepperoni']}
+];
+
+const mappedPizzas = pizzas.map((pizza, index) => pizza.name.toUpperCase() + index);
+
+console.log(mappedPizzas);
+
+const pizza = {
+    name: 'Blazing Inferno',
+    getName: function () {
+        setTimeout(function () {
+            console.log('timeout log', this);
+        }, 100);
+        console.log('getName log', this);
+    }
+};
+
+console.log('method call log', pizza.getName());
