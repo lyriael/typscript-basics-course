6901318b0aea11b1ab8939566ed8cf85ac5f851f
Judith Fuog <lyriael@gmx.net>
2020-05-06 18:42:06 +0200

    TS-Basics: 25. Typed Functions and Default Params

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,8 @@
 let sumOrder: (price: number, quantity?: number) => number;
 
-sumOrder = (x, y) => {
-    if (y) {
-        return x * y
-    }
-    return x;
+sumOrder = (x, y= 1) => {
+    const q = y || 1;
+    return x * q
 };
 
 const sum = sumOrder(25);
