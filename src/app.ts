27fe7d447e6a2d68d4f2813f16166535022579c1
Judith Fuog <lyriael@gmx.net>
2020-05-06 10:21:20 +0200

    TS-Basics: 11. Array spread operator

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-function sumAll(message: string, ...arr: Array<number>) {
-    console.log(message);
-    return arr.reduce(((previousValue, currentValue) => previousValue + currentValue));
-}
+const toppings = ['beacon', 'chilli'];
+const newToppings = ['pepperoni'];
 
-console.log(sumAll('here you go: ', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+const allTopping = [...toppings, ...newToppings];
+const allToppingsDifferentOrder = [...newToppings, ...toppings];
+
+console.log(allTopping);
+console.log(allToppingsDifferentOrder);
