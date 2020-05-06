0e56be7258ff3b1b9d3f5b5bf8026338cbcaf445
Judith Fuog <lyriael@gmx.net>
2020-05-06 17:42:45 +0200

    TS-Basics: 19. Void Type

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,8 @@
-let implicitCoupon = 'pizza25'; // inferring the type of string
-let explicitCoupon: string = 'pizza25';
+let selectedTopping: string = 'pepperoni';
+
+function selectTopping(topping: string): void {
+    selectedTopping = topping; // btw this is a "not pure" function, because it uses the variable from outside
+}
+
+selectTopping('bacon');
+console.log(selectedTopping);
