6ff5bb573821de6e6387c508546baf75bf70a2f8
Judith Fuog <lyriael@gmx.net>
2020-05-06 18:29:58 +0200

    TS-Basics: 23. Function Types

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
-let pizzaSize: string = 'small';
-
-function selectSize(size: 'small' | 'medium' | 'large'): void {
-    pizzaSize = size;
+// named function
+function sumOrder(price: number, quantity: number): number {
+    return price * quantity;
 }
 
-selectSize('medium');
+// if eg overwritting a function from a library
+// let sumOrder2: Function; // use this if you don't know how the signature will be like
+let sumOrder2: (price: number, quantity: number ) => number;
 
-console.log(`Pizza size: ${pizzaSize}`);
+sumOrder2 = (x, y) => x * y;
+
+sumOrder2 = (price: number, quantity: number): number => {
+    return price * quantity;
+};
+
+const sum = sumOrder2(25, 2);
