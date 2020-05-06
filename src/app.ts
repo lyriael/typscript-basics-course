468d83c1642008e71866ef8f0fec84a73ce5bdde
Judith Fuog <lyriael@gmx.net>
2020-05-06 18:35:49 +0200

    TS-Basics: 24. Functions and Optional Arguments

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,10 @@
-// named function
-function sumOrder(price: number, quantity: number): number {
-    return price * quantity;
-}
+let sumOrder: (price: number, quantity?: number) => number;
 
-// if eg overwritting a function from a library
-// let sumOrder2: Function; // use this if you don't know how the signature will be like
-let sumOrder2: (price: number, quantity: number ) => number;
-
-sumOrder2 = (x, y) => x * y;
-
-sumOrder2 = (price: number, quantity: number): number => {
-    return price * quantity;
+sumOrder = (x, y) => {
+    if (y) {
+        return x * y
+    }
+    return x;
 };
 
-const sum = sumOrder2(25, 2);
+const sum = sumOrder(25);
