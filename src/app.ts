37af2d876eb37c6b70d52eb564956d11b4231ae8
Judith Fuog <lyriael@gmx.net>
2020-05-07 10:34:45 +0200

    TS-Basics: 31. Creating Interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
-type Pizza = { name: string, toppings: number };
-
-const pizza: Pizza = {name: 'Blazing Inferno', toppings: 5};
-
-const serialized = JSON.stringify(pizza);
-
-function getNameFromJSON(obj: string): string {
-    // Old way
-//     return (<Pizza>JSON.parse(obj)).name;
-    return (JSON.parse(obj) as Pizza).name;
+interface Pizza {
+    name: string;
+    sizes: string[];
 }
 
+let pizza: Pizza;
+
+function createPizza(name: string, sizes: string[]): Pizza {
+    return {
+        name,
+        sizes,
+    };
+}
+
+pizza = createPizza('Pepperoni', ['small', 'medium']);
